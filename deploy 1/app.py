258e852c44b1ff23494cfabbc7c000cac1978d5f
Kevin Gunawan <95064835+KevinGunawan13@users.noreply.github.com>
2021-12-12 13:48:07 +0800

    Add files via upload

---
+++ deploy 1/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.haha', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html', Total_timbulan_sampah=0)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    Predict the insurance cost based on user inputs
+    and render the result to the html page
+    '''
+    pengurangansampahtahunan, penanganansampahtahunan = [x for x in request.form.values()]
+
+    data = []
+
+    data.append(float(pengurangansampahtahunan,penanganansampahtahunan))
+    
+    
+    prediction = model.predict([data])
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', Total_timbulan_sampah=output, pengurangansampahtahunan=pengurangansampahtahunan, penanganansampahtahunan=penanganansampahtahunan)
+
+if __name__ == '__main__':
+    app.run(debug=True)
